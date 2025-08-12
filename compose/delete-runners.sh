678ee774039a6cf3a40f37822ca27add079ad6c4
John Widmaier <johnwid92@gmail.com>
2025-08-12 11:20:56 -0400

    increase resources, add delete-runner script

---
+++ compose/delete-runners.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# REPO=$1
+# TOKEN=$2
+# set -a            # automatically export all variables
+# source .env       # or `. .env`
+# set +a            # stop automatic export
+
+source ./\.env
+echo "REPO ${GH_REPO}"
+REPO="${GH_REPO}"
+TOKEN="${GH_PAT}"
+
+RUNNER_LIST=$(curl -H "Authorization: token ${TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners | jq '[.runners[] | select(.status | contains("offline")) | {id: .id}]')
+echo "RUNNER_LIST ${RUNNER_LIST}"
+for id in $(echo "$RUNNER_LIST" | jq -r '.[] | @base64'); do
+        _jq() {
+                echo ${id} | base64 --decode | jq -r ${1}
+        }
+        echo $(_jq '.id')
+        curl -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: token ${TOKEN}"  https://api.github.com/repos/${REPO}/actions/runners/$(_jq '.id')
+done
